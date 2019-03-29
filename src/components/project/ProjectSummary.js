eab8e65f0e5f01b50af85a1359bd2f49867151df
Sinha <sinha@Sinhas-MacBook-Air.local>
2019-03-30 00:43:53 +0530

    change wall

--- src/components/project/ProjectSummary.js
+++ src/components/project/ProjectSummary.js
@@ -4,12 +4,14 @@ import moment from 'moment'
 const ProjectSummary = ({project}) => {
     return (
       <div className="project-list section">
-        <div className="card z-depth-0 project-summary">
-            <div className="card-content grey-text text-darken-3">
-                <span className="card-title">{project.title}</span>
-                <p>{project.content}</p>
+        <div className="card z-depth-0 project-summary ">
+            <div className= "card-content grey-text text-darken-3" >
+            <div class="card-panel yellow">
+                <span className="card-title" >{project.title}</span>
+                <p >{project.content}</p>
                 <p>Posted by {project.authorFirstName} {project.authorLastName}</p>
                 <p className="grey-text">{moment(project.createdAt.toDate()).calendar()}</p>
+                </div>
             </div>
         </div>
       </div>
