4ba9606c97cf0db5f33f709565bb82cc46bd0363
rr250 <rr250@snu.edu.in>
2019-04-13 03:49:48 +0530

    Updated Maps

--- src/components/maps/CurrentLocation.js
+++ src/components/maps/CurrentLocation.js
@@ -5,7 +5,7 @@ const mapStyles = {
   map: {
     position: 'absolute',
     width: '100%',
-    height: '100%'
+    height: '93.6%'
   }
 };
 export class CurrentLocation extends React.Component {
--- src/components/maps/Maps.js
+++ src/components/maps/Maps.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
-
-import CurrentLocation from './CureentLocation';
+import CurrentLocation from './CurrentLocation';
 
 export class Maps extends Component {
   state = {
@@ -27,9 +26,14 @@ export class Maps extends Component {
   };
 
   render() {
+    var icon1 = {
+      url: "https://cdn4.iconfinder.com/data/icons/ghost-face-1/600/264_happy_surprised_emoji_emoticon_smiley_ghost-512.png", // url
+      scaledSize: new this.props.google.maps.Size(60, 60), // scaled size
+    };
     return (
       <CurrentLocation centerAroundCurrentLocation google={this.props.google}>
         <Marker onClick={this.onMarkerClick} name={'current location'} />
+        <Marker onClick={this.onMarkerClick} name={'Blue Circle'} icon={icon1} scaledSize={ new this.props.google.maps.Size(90, 42)} position={{lat: 28.527083333333334 , lng: 77.57249999999999}} />
         <InfoWindow
           marker={this.state.activeMarker}
           visible={this.state.showingInfoWindow}
