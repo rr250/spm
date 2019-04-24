778e52ad6bcc17e328f1552198092140ff480a04
rr250 <rr250@snu.edu.in>
2019-04-24 14:39:04 +0530

    hosted on firebase

---
+++ dist/precache-manifest.9a6bc6f28018f0d70c103b72ed212a7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "55920c21f097f180abaa",
+    "url": "/static/js/main.55920c21.chunk.js"
+  },
+  {
+    "revision": "21388b18e140b63df1a8",
+    "url": "/static/js/1.21388b18.chunk.js"
+  },
+  {
+    "revision": "55920c21f097f180abaa",
+    "url": "/static/css/main.313813a5.chunk.css"
+  },
+  {
+    "revision": "9d06eef10b7f23c27b3b60792c5c9c7b",
+    "url": "/index.html"
+  }
+];
